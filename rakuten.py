83ba1e20d87998e9a4dfe0f54b548938995295a4
low224 <57149209+low224@users.noreply.github.com>
2019-12-27 23:52:16 +0800

    Add files via upload
    Bug fix

--- rakuten.py
+++ rakuten.py
@@ -1,4 +1,5 @@
 # Utility to calculate Rakuten Trade fees
+from math import ceil
 
 print('Please input total share number to purchase ')
 share_no = int(input())
@@ -6,8 +7,8 @@ share_no = int(input())
 print('Please input purchse price')
 market_price = float(input())
 
-print('Stamp Duty exemption? Y ? N')
-market_exempt = True if input() == 'Y' else False
+print('Stamp Duty exemption? y / n')
+market_exempt = True if input() == 'y' else False
 
 total_price = share_no * market_price
 
@@ -22,15 +23,24 @@ else:
 	broker_fee = 100
 
 # Calulate clearing fee
-clearing_fee = min(total_price * 0.003, 1000)
+clearing_fee = float(min(total_price * 0.0003, 1000))
 
 # Calculate stamp duty
-if market_exempt is False:
+if market_exempt is True:
 	stamp_duty = 0
 else:
-	stamp_duty = max(1, int(total_price / 1000))
+	stamp_duty = ceil(total_price / 1000)
 	stamp_duty = min(200, stamp_duty)
 
-total_fee = total_price + clearing_fee + stamp_duty
+# Calculate sst
+sst = broker_fee * 0.06
 
+total_cost = clearing_fee + stamp_duty + broker_fee + sst
+total_fee = total_price + clearing_fee + stamp_duty + broker_fee + sst
+
+print('The total broker fee is %.2f' % broker_fee)
+print('The total clearing fee is %.2f' % clearing_fee)
+print('The total stamp duty is %.2f' % stamp_duty)
+print('The total SST is %.2f' % sst)
+print('The total transaction cost is %.2f' % total_cost)
 print('The total fee is : %.2f' % total_fee)
